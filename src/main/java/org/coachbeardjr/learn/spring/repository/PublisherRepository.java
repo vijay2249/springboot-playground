39fbad14dd8c2120017496d8c22250543cfa2fec
Coach Beard Jr <vijaychowdary2249@pm.me>
2025-02-11 18:27:57 +0200

    fix insert query issue (override default save method coz its considering update rather save)

--- src/main/java/org/coachbeardjr/learn/spring/repository/PublisherRepository.java
+++ src/main/java/org/coachbeardjr/learn/spring/repository/PublisherRepository.java
@@ -1,8 +1,14 @@
 package org.coachbeardjr.learn.spring.repository;
 
 import org.coachbeardjr.learn.spring.model.Publisher;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
+import reactor.core.publisher.Mono;
+
 public interface PublisherRepository extends R2dbcRepository<Publisher, Long>{
-  
+
+  @Override
+  @Query("insert into publishers (id, name, address) values (:#{#publisher.id}, :#{#publisher.name}, :#{#publisher.address})")
+  Mono<Publisher> save(Publisher publisher);
 }
