ef355aae719c1e3f9319137c79188ae9ce8c593e
Coach Beard Jr <vijaychowdary2249@pm.me>
2025-02-12 04:45:20 +0200

    fix Table annotation import

--- src/main/java/org/coachbeardjr/learn/spring/model/Author.java
+++ src/main/java/org/coachbeardjr/learn/spring/model/Author.java
@@ -1,15 +1,16 @@
 package org.coachbeardjr.learn.spring.model;
 
+import org.springframework.data.relational.core.mapping.Table;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name="authors")
+@Table("authors")
 @Getter
 @Setter
 public class Author {
--- src/main/java/org/coachbeardjr/learn/spring/model/Book.java
+++ src/main/java/org/coachbeardjr/learn/spring/model/Book.java
@@ -2,16 +2,17 @@ package org.coachbeardjr.learn.spring.model;
 
 import java.util.List;
 
+import org.springframework.data.relational.core.mapping.Table;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name="books")
+@Table("books")
 @Getter
 @Setter
 public class Book{
